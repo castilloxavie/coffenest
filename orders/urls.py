74f7d7cd878a6122388920fb489d6f7e6b2dd30d
castilloxavie <xcastillovaron93@gmail.com>
2024-09-24 23:53:12 -0500

    update  and create of files

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,5 @@ from .views import CreateOrderProductView, MyOrderView
 
 urlpatterns = [
     path("mi_orden/", MyOrderView.as_view(), name="my_order"),
-    path("agregar_producto", CreateOrderProductView.as_view(), name="add_product"),
+    path("agregar_producto/", CreateOrderProductView.as_view(), name="add_product"),
 ]
